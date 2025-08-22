0fd38dc1dc34bab4b2f55acd4740f8baab7fc3c2
AveryClapp <avery.clapp@gmail.com>
2025-08-22 18:08:36 -0400

    adjust byte alignment for Order struct

--- include/core/Order.h
+++ include/core/Order.h
@@ -7,13 +7,13 @@
 struct Level;
 
 struct Order {
-  Price price;
-  OrderType type;
-  Direction direction;
-  Quantity initial_quantity;
-  Quantity remaining_quantity;
-  Time time;
-  Level *cur_level;
-  ID id;
-  size_t level_position;
+  Price price;                 // 4 Bytes
+  Quantity initial_quantity;   // 4 Bytes
+  Quantity remaining_quantity; // 4 Bytes
+  ID id;                       // 8 Bytes
+  size_t level_position;       // 8 Bytes
+  Time time;                   // 8 bytes
+  Level *cur_level;            // 30 + Bytes
+  OrderType type;              // 1 Byte
+  Direction direction;         // 1 Byte
 };
